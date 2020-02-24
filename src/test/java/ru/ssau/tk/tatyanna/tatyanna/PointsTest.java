7297172cb1856e00bd3a4682a70bdd935f66b492
Martynova <tatyanamartynova01@mail.ru>
2020-02-24 04:39:26 +0400

    3.15

--- src/test/java/ru/ssau/tk/tatyanna/tatyanna/PointsTest.java
+++ src/test/java/ru/ssau/tk/tatyanna/tatyanna/PointsTest.java
@@ -1,6 +1,7 @@
 package ru.ssau.tk.tatyanna.tatyanna;
 
 import org.junit.jupiter.api.Test;
+
 import ru.ssau.tk.tatyanna.tatyanna.Points;
 import ru.ssau.tk.tatyanna.tatyanna.Point;
 import static org.junit.jupiter.api.Assertions.*;
